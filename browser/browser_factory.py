139fcf070be3c244e4900885de880bda447ba08f
Hubert Zajac <hubertzajac6@gmail.com>
2023-05-02 13:52:21 +0200

    browser factory - chrome support

---
+++ browser/browser_factory.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from browser_type import BrowserType
+
+
+def create_browser(browser_type: BrowserType = BrowserType.CHROME) -> webdriver:
+    match browser_type:
+        case BrowserType.CHROME:
+            return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
