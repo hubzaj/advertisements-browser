15cd74a654f550b2981627e8a2448591416c7857
Hubert Zajac <hubertzajac6@gmail.com>
2023-05-02 15:49:40 +0200

    adjustment

--- browser/browser_type.py
+++ browser/browser_type.py
@@ -1,5 +1,13 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class BrowserType(Enum):
-    CHROME = 'chrome'
+    CHROME = auto()
+    CHROME_IN_DOCKER = auto()
+
+    @classmethod
+    def get_browser(cls, name: str) -> 'BrowserType':
+        try:
+            return cls[name.upper()]
+        except KeyError:
+            raise KeyError(f"[{name}] browser isn't supported - choose one of {list(map(str, cls))}")
