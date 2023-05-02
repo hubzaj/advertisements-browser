3e68e64bc3d903461cfe3be13ca123815a96cfa1
Hubert Zajac <hubertzajac6@gmail.com>
2023-05-02 16:10:23 +0200

    unit test

---
+++ browser/test/browser_type_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from browser.browser_type import BrowserType
+
+
+class BrowserTypeTest(TestCase):
+
+    def test_get_supported_browser_type(self):
+        self.assertIsNotNone(BrowserType.get_browser("chrome"))
+
+    def test_unsupported_browser_type(self):
+        self.assertRaises(KeyError, lambda: BrowserType.get_browser("IE"))
