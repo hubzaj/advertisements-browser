4fd370dfeb0f92358d7b05fa587d546f3fb2f9f8
Hubert Zajac <hubertzajac6@gmail.com>
2023-05-02 14:43:38 +0200

    adjustment

--- browser/browser_factory.py
+++ browser/browser_factory.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.remote.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 from .browser_type import BrowserType
 
 
-def create_browser(browser_type: BrowserType = BrowserType.CHROME) -> webdriver:
+def create_browser(browser_type: BrowserType = BrowserType.CHROME) -> WebDriver:
     match browser_type:
         case BrowserType.CHROME:
-            driver: webdriver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            driver: WebDriver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
             driver.maximize_window()
             return driver
