106e28bc81657d0ccc236c192ba6200b7505a8b8
Hubert Zajac <hubertzajac6@gmail.com>
2023-07-29 16:03:56 +0200

    web driver manager

--- browser/browser_factory.py
+++ browser/browser_factory.py
@@ -1,5 +1,6 @@
 from seleniumwire import webdriver
 from seleniumwire.webdriver import Chrome
+from webdriver_manager.chrome import ChromeDriverManager
 
 from browser.browser import Browser
 from browser.browser_type import BrowserType
@@ -8,15 +9,15 @@ from browser.browser_type import BrowserType
 def create_browser(browser_type: BrowserType = BrowserType.CHROME) -> Browser:
     match browser_type:
         case BrowserType.CHROME:
+            ChromeDriverManager().install()
             options: webdriver.ChromeOptions = webdriver.ChromeOptions()
-            # options.binary_location = ChromeDriverManager().install()
             driver: Chrome = webdriver.Chrome(options=options)
             driver.maximize_window()
             return Browser(driver)
         case BrowserType.CHROME_HEADLESS:
+            ChromeDriverManager().install()
             options: webdriver.ChromeOptions = webdriver.ChromeOptions()
-            options.headless = True
-            # options.binary_location = ChromeDriverManager().install()
+            options.add_argument('--headless')
             driver: Chrome = webdriver.Chrome(options=options)
             driver.maximize_window()
             return Browser(driver)
